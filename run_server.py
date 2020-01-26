ee579f9157903cc672dea45aff48a64714ba1f23
Gerardo Gutiérrez <muzgash@gmail.com>
2020-01-26 20:45:06 +0100

    added colciencias endpoints

--- run_server.py
+++ run_server.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     server=PDBServer(dbname=args.db,ip=ip,port=args.port)
     server.create_endpoints('redalyc')
     server.create_endpoints('udea')
+    server.create_endpoints('colciencias')
     server.start()
 
 
